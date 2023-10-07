f0f24e1c4750a1951d513e401139e41e602d5273
hbreck <hbreckenridge@yahoo.com>
2023-10-06 20:04:21 -0400

    Added methods to check for and count vowels in a string

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String input = "aaa";
 
-        checkVowel(input);
+        vowelCountArraylist(input);
     }
 
     public static String reverseStringStringBuilder(String str){
@@ -28,6 +28,46 @@ public class Main {
                 System.out.println("Given string contains " + input.charAt(i) + " at the index " + i);
             }
         }
+    }
+
+    public static int vowelCountIter(char chars)
+    {
+        if (chars == 'a' || chars == 'e' || chars == 'i'
+                || chars == 'o' || chars == 'u') {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static int vowelCountRecur(String str, int l)
+    {
+        if (l == 1) {
+            return vowelCountIter(str.charAt(l - 1));
+        }
+
+        return vowelCountRecur(str, l - 1)
+                + (str.charAt(l - 1));
+    }
+
+    public static void vowelCountArraylist(String str){
+        str = str.toLowerCase();
+        int count = 0;
+        String vow ="aeiou";
+        ArrayList<Character> vowels = new ArrayList<Character>();
+        for(int i=0;i<vow.length();i++)
+        {
+            vowels.add(vow.charAt(i));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if(vowels.contains(str.charAt(i))){
+                count++;
+            }
+        }
+
+        // display total count of vowels in string
+        System.out.println("Total no of vowels in string are: " + count);
     }
 
 
